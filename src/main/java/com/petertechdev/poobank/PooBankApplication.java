aaf66b588719107e68487d050f492a7c1acfcee4
petertechdev <peterleite.dev@gmail.com>
2022-11-19 17:21:45 -0300

    initial files

---
+++ src/main/java/com/petertechdev/poobank/PooBankApplication.java
@@ -0,0 +1,13 @@
+package com.petertechdev.poobank;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PooBankApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PooBankApplication.class, args);
+	}
+
+}
